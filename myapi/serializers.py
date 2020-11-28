27da37a585d0b4335a05a526b710225df945dfac
iofall <50991099+iofall@users.noreply.github.com>
2020-11-29 03:27:20 +0530

    Initial Commit

---
+++ myapi/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+import spacy
+
+from .models import ClientText, Keyword
+
+def some(x):
+    for t in x:
+        obj = Keyword(text = t, word="a", entity_type="b")
+        obj.save()
+
+class ClientTextKeyWords(serializers.ModelSerializer):
+    class Meta:
+        model = Keyword
+        fields = ['text', 'word', 'entity_type']
+
+class ClientTextSerializer(serializers.ModelSerializer):
+    keywords = ClientTextKeyWords(many=True, required=False)
+    
+    class Meta:
+        model = ClientText
+        fields = ['url', 'id', 'text', 'keywords']
+
+    def create(self, validated_data):
+        client_text = ClientText.objects.create(**validated_data)
+
+        nlp = spacy.load("en_core_web_sm")
+
+        doc = nlp(client_text.text)
+
+        for ent in doc.ents:
+            Keyword.objects.create(**{"text":client_text, "word":ent.text, "entity_type":ent.label_})
+            
+        return client_text
+
