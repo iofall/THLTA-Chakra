3c2b735dab7c900457ba43bc1e2ef855b14c89bf
iofall <50991099+iofall@users.noreply.github.com>
2020-11-29 13:20:36 +0530

    Heroku Deploy

--- Chakra/settings.py
+++ Chakra/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a^8ovhn%ly@0%z+_^#c30e@$+$sdyo5)=o9pb_y_csg!did1@^'
+SECRET_KEY = os.environ.get('CHAKRA_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://team-chakra-thlta.herokuapp.com/']
 
 
 # Application definition
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals(), allowed_hosts=False, secret_key=False)
