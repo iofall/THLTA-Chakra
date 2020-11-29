d3fb8526e96a6fd4c2d5f69dbd70a0668fc4a517
iofall <50991099+iofall@users.noreply.github.com>
2020-11-29 13:40:40 +0530

    Heroku fix hosts

--- Chakra/settings.py
+++ Chakra/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('CHAKRA_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG') == 'True')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://team-chakra-thlta.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'team-chakra-thlta.herokuapp.com']
 
 
 # Application definition
